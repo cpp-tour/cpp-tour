774793b1327892ec61cb352da37e642ba4fdd842
Arvid Gerstmann <dev@arvid-g.de>
2017-12-03 22:43:42 +0100

    Improve deployment script

--- ci/update_pages.sh
+++ ci/update_pages.sh
@@ -1,37 +1,46 @@
 #!/bin/bash
+set -v
 
-##Requires environment variable GIT_TOKEN to be set.
-if [ "$TRAVIS_BRANCH" -ne "master"] && [ "$TRAVIS_PULL_REQUEST" -eq "false" ]
+# Requires environment variable GIT_TOKEN to be set.
+if [ "$TRAVIS_BRANCH" != "master" ] || \
+   [ "$TRAVIS_PULL_REQUEST" = "true" ] || \
+   [ "$GIT_TOKEN" = "" ];
+then
     exit 0
 fi
 
-REPO=`git config remote.origin.url`
-SHA=`git log -1 --format="%s(%h %cd)" --date=short`
-COMMIT_AUTHOR_EMAIL=`git --no-pager log -1 --pretty=format:"%ae" HEAD`
+# Create commit message
+REPO=$(git config remote.origin.url)
+HASH=$(git log --oneline | grep "\[TRAVIS\]" | awk '{print $1}' | head -n1)
+DESC=$(git describe --tags --always --dirty)
+LOG=$(git log --pretty="- "%s --first-parent $HASH..HEAD)
+MSG=$(echo -ne "[TRAVIS] Auto deployment of ${HASH}:\n\n${LOG}\n")
 
-git clone $REPO out/
-
-cd out/
-
+# Clone the repo
+git clone $REPO _out/
+cd _out/
 git checkout gh-pages || git checkout --orphan gh-pages
 
-ls
-
+# Replace contents with the build contents
 rm -rf *
-
 cp -rf ../public/* .
 
-# In case some temporary files
+# Remove temporary files
 find . -name "_*.*" | xargs rm -rf
 find . -name ".*.*" | xargs rm -rf
 
-git status
-
+# Setup credentials
 git config user.name "Travis CI"
-git config user.email "$COMMIT_AUTHOR_EMAIL"
+git config user.email "github@arvid.io"
 echo "https://${GIT_TOKEN}:x-oauth-basic@github.com\n" > ~/.git-credentials
 git config remote.origin.url "https://${GIT_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git"
 
-git add --all .
-git commit -m "Auto-update" -m "Commit: ${SHA}"
+# Push to repo
+git add -A
+git commit -am "${MSG}"
 git push origin HEAD
+
+# Remove artifacts again
+cd ..
+rm -rf _out/
+
