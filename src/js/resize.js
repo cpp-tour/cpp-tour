968f7332dc1c7a8ab3c3a65cb4d81efbe9d2096b
srbraun <2sergebraun@gmail.com>
2017-12-21 15:22:37 +0100

    delited reload, rewrote Split for flexbox

--- src/js/resize.js
+++ src/js/resize.js
@@ -4,19 +4,29 @@ const PAGE_WIDTH = 960
 
 const isSplited = () => document.getElementsByClassName('gutter').length > 0
 const isBigScreen = () => window.innerWidth > PAGE_WIDTH
+const needGutter = () => isBigScreen() && !isSplited()
+const noNeedGutter = () => !isBigScreen() && isSplited()
+
 const main = document.getElementById('main')
 const gutter = () => document.getElementsByClassName('gutter')[0]
 
 const makeGutter = () => {Split(['#left_pane', '#right_pane'], {
-    gutterSize: 8,
-    cursor: 'col-resize'
-  })}
+    elementStyle: function (dimension, size, gutterSize) {
+        return {
+            'flex-basis': 'calc(' + size + '% - ' + gutterSize + 'px)'
+        }
+    },
+    gutterStyle: function (dimension, gutterSize) {
+        return {
+            'flex-basis':  gutterSize + 'px'
+        }
+    }
+})}
 
 export default function resize() {
-    if (isBigScreen() && !isSplited()) {
+    if (needGutter()) {
         makeGutter()
-    } else if (!isBigScreen() && isSplited()) {
+    } else if (noNeedGutter()) {
         main.removeChild(gutter())
-        location.reload()
     }
 }
