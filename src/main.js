96889858193457ba30001400606576b3d95f31f9
srbraun <2sergebraun@gmail.com>
2017-12-21 16:03:12 +0100

    changes from remote

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 "use strict"
 
 import UI from './js/UI.js'
+import resize from './js/resize.js'
 
 import './styles/main.sss'
 
 window.addEventListener("load", function() {
-    const CODE = "#include <iostream>\n\nint main() {\n\tstd::cout << \"Hello, world!\";\n\treturn 0;\n}"
     const ui = new UI()
 
     /*const code_editor =*/ ui.create_editor(CODE)
+
+    resize()
+    window.addEventListener('resize', resize)
+
 })
