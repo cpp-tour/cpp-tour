27ac686f83aefe5998d2d681ee901b4c2796a161
srbraun <2sergebraun@gmail.com>
2017-12-21 21:38:29 +0100

    + vertical split and scroll for output

--- src/js/resize.js
+++ src/js/resize.js
@@ -2,31 +2,30 @@ import Split from 'split.js'
 
 const PAGE_WIDTH = 960
 
-const isSplited = () => document.getElementsByClassName('gutter').length > 0
-const isBigScreen = () => window.innerWidth > PAGE_WIDTH
-const needGutter = () => isBigScreen() && !isSplited()
-const noNeedGutter = () => !isBigScreen() && isSplited()
-
-const main = document.getElementById('main')
-const gutter = () => document.getElementsByClassName('gutter')[0]
-
-const makeGutter = () => {Split(['#left_pane', '#right_pane'], {
-    elementStyle: function (dimension, size, gutterSize) {
-        return {
-            'flex-basis': 'calc(' + size + '% - ' + gutterSize + 'px)'
+const makeGutter = () => {
+    Split(['#left_pane', '#right_pane'], {
+        cursor: 'col-resize',
+        elementStyle: function (dimension, size, gutterSize) {
+            return {
+                'flex-basis': 'calc(' + size + '% - ' + gutterSize + 'px)'
+            }
+        },
+        gutterStyle: function (dimension, gutterSize) {
+            return {
+                'flex-basis':  gutterSize + 'px'
+            }
         }
-    },
-    gutterStyle: function (dimension, gutterSize) {
-        return {
-            'flex-basis':  gutterSize + 'px'
-        }
-    }
-})}
+    })
+}
 
 export default function resize() {
-    if (needGutter()) {
+    const gutter = document.getElementsByClassName('gutter')[0]
+    const is_split = gutter !== undefined
+    const is_big_screen = window.innerWidth > PAGE_WIDTH
+
+    if (is_big_screen && !is_split) {
         makeGutter()
-    } else if (noNeedGutter()) {
-        main.removeChild(gutter())
+    } else if (!is_big_screen && is_split) {
+        gutter.parentNode.removeChild(gutter)
     }
 }
