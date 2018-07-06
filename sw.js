9541c28c568bae560c4b444c311d7506eef376e2
Travis CI (from Travis CI) <github@arvid.io>
2018-07-06 13:46:33 +0000

    Deploy cpp-tour to github.com/cpp-tour/cpp-tour.git:gh-pages

---
+++ sw.js
@@ -0,0 +1,31 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("/workbox-v3.3.1/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "/workbox-v3.3.1"});
+
+importScripts(
+  "/precache-manifest.4e124866589f5e2de559d498e8678572.js"
+);
+
+workbox.skipWaiting();
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
